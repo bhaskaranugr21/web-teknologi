92ac81ebc109b3d911556cf4cc47a9f7adfc61f2
Bagaskara <bagaskarbay21@gmail.com>
2024-11-23 17:27:54 +0700

    update commit

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
-import AcmeLogo from '@/app/ui/acme-logo';
+//import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
 import { signOut } from '@/auth';
+
 export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
@@ -12,7 +13,14 @@ export default function SideNav() {
         href="/"
       >
         <div className="w-32 text-white md:w-40">
-          <AcmeLogo />
+          {/*<AcmeLogo />*/}
+          <img
+          src="/logo/uia-logo.png"
+          width={300}
+          height={160}
+          className="hidden md:block"
+          alt="Banner UIA"
+          />
         </div>
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
